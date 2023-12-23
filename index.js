bf0c6fd2ea6edab4e753a4b13fc01fb6ba81f7e8
rhythmdev <rhythmbiswas@gmail.com>
2023-12-23 18:42:43 +0600

    books category post and get api make and test done

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 const express = require('express');
 const cors = require('cors');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hjmkubl.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const booksCategoryCollection = client.db('openShelves').collection('booksCategory')
+         
+        // get books category
+        app.get('/booksCategory', async (req, res) => {
+            const result = await booksCategoryCollection.find().toArray();
+            res.send(result)
+        })
+
+
+        // for books categories post api
+        app.post('/booksCategory', async (req, res) => {
+            const booksCategory = req.body;
+            const result = await booksCategoryCollection.insertOne(booksCategory);
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Open Shelves Server Is Running')
